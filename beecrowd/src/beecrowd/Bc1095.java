c014da88aa7adb3f07e2495b5ac72d262d1dffe2
Samuel <108034309+SamColt36@users.noreply.github.com>
2023-01-28 09:22:13 -0300

    Add files via upload

---
+++ beecrowd/src/beecrowd/Bc1095.java
@@ -0,0 +1,16 @@
+package beecrowd;
+
+public class Bc1095 {
+
+	public static void main(String[] args) {
+		int j = 60;
+		for (int i = 1; i < 100; i = i + 3) {
+			System.out.println("I=" + i + " J=" + j);
+			j -= 5;
+			if (j == -5) {
+				break;
+			}
+		}
+	}
+
+}
